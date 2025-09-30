bb7203cc2665030f68f833b1309e097d446697c5
King Yeh <69230356+xbowery@users.noreply.github.com>
2025-09-30 11:45:43 +0800

    Change default asset class and adjust DataFrame columns
    Updated default asset class selection and modified DataFrame columns based on asset class.

--- web_viewer.py
+++ web_viewer.py
@@ -14,7 +14,7 @@ st.title('DTCC Swap Data Repository Viewer')
 
 st.write("This is a prototype of a viewer for the DTCC Swap Data Repository (taken from the latest data available in the repository).")
 
-asset = st.multiselect('Choose Your Asset Class: ', ['Commodities', 'Credits', 'Forex', 'Rates'], ['Commodities'])
+asset = st.multiselect('Choose Your Asset Class: ', ['Commodities', 'Credits', 'Forex', 'Rates'], ['Rates'])
 
 start_date = st.date_input('Select the starting date: ', datetime.today() - timedelta(days=2))
 end_date = st.date_input('Select the ending date: ', datetime.today())
@@ -49,12 +49,22 @@ if button:
     except:
         pass
     
-    df = df[['_id', 'Asset Class', 'Trade Structure',
-            'Effective Date', 'Event timestamp', 'Exchange rate', 'Exchange rate basis', 'Execution Timestamp',
-            'Expiration Date', 'Fixed rate-Leg 1', 'Fixed rate-Leg 2', 'Notional amount-Leg 1',
-            'Notional amount-Leg 2', 'Notional currency-Leg 1', 'Notional currency-Leg 2',
-            'Option Premium Amount', 'Option Premium Currency', 'Option Style', 'Option Type',
-            'Strike Price', 'Strike price currency/currency pair']]
+    if df['Asset Class'].unique()[0] != 'Rates':
+        df = df[['_id', 'Asset Class', 'Trade Structure',
+                'Effective Date', 'Event timestamp', 'Exchange rate', 'Exchange rate basis', 'Execution Timestamp',
+                'Expiration Date', 'Fixed rate-Leg 1', 'Fixed rate-Leg 2', 'Notional amount-Leg 1',
+                'Notional amount-Leg 2', 'Notional currency-Leg 1', 'Notional currency-Leg 2',
+                'Option Premium Amount', 'Option Premium Currency', 'Option Style', 'Option Type',
+                'Strike Price', 'Strike price currency/currency pair']]
+    else:
+        df = df[['_id', 'Asset Class', 'UPI Underlier Name', 'Effective Date', 'Event timestamp', 'Exchange rate', 'Exchange rate basis', 'Execution Timestamp',
+                'Expiration Date', 'First exercise date', 'Fixed rate-Leg 1', 'Fixed rate-Leg 2', 'Notional amount-Leg 1',
+                'Notional amount-Leg 2', 'Notional currency-Leg 1', 'Notional currency-Leg 2',
+                'Notional amount in effect on associated effective date-Leg 1', 'Notional amount in effect on associated effective date-Leg 2',
+                'Effective date of the notional amount-Leg 1', 'Effective date of the notional amount-Leg 2',
+                'End date of the notional amount-Leg 1', 'End date of the notional amount-Leg 2',
+                'Option Premium Amount', 'Option Premium Currency', 'Spread-Leg 1', 'Spread-Leg 2',
+                'Strike Price', 'Strike price currency/currency pair']]
     
     df = df.sort_values(by=['_id']).reset_index(drop=True)
     st.dataframe(df)
